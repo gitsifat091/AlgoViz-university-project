104f70c3bbb85560a748d0ba9cf92e59a498a920
gitsifat091 <abdussamadsifat91@gmail.com>
2026-04-04 17:34:44 +0600

    Clean up - remove deployment config

--- algoviz/settings.py
+++ algoviz/settings.py
@@ -127,14 +127,14 @@ LOGIN_URL = '/login/'
 
 
 import os
-# import dj_database_url
-try:
-    import dj_database_url
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    if DATABASE_URL:
-        DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
-except ImportError:
-    pass
+import dj_database_url
+# try:
+#     import dj_database_url
+#     DATABASE_URL = os.environ.get('DATABASE_URL')
+#     if DATABASE_URL:
+#         DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
+# except ImportError:
+#     pass
 
 # Static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
